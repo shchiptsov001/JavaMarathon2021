768dbcd1267d0775ff0e8beae985795f283a3b35
Sergey <stepoffozel@gmail.com>
2021-02-24 22:45:18 +0300

    Add solution Day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,17 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
 
+        while (y != 0) {
+            System.out.println(x / y);
+            x = scanner.nextDouble();
+            y = scanner.nextDouble();
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x;
+        double y;
 
+        for (int i = 0; i < 5; i++) {
+            x = scanner.nextDouble();
+            y = scanner.nextDouble();
+            if (y == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(x / y);
+        }
     }
 }
